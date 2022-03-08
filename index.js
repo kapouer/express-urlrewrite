3bc12aec46b31ddf81eac74b244661a32b7f29ee
Jérémy Lal <kapouer@melix.org>
2022-03-08 10:32:18 +0100

    Linting

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
  * Module dependencies.
  */
 
- var debug = require('debug')('express-urlrewrite');
- var toRegexp = require('path-to-regexp');
- var URL = require('url');
+const debug = require('debug')('express-urlrewrite');
+const { pathToRegexp } = require('path-to-regexp');
+const URL = require('url');
 
 /**
  * Expose `expose`.
@@ -23,43 +23,44 @@ module.exports = rewrite;
  */
 
 function rewrite(src, dst) {
-  var keys = [], re, map;
+	const keys = [];
+	let re, map;
 
-  if (dst) {
-    re = toRegexp(src, keys);
-    map = toMap(keys);
-    debug('rewrite %s -> %s    %s', src, dst, re);
-  } else {
-    debug('rewrite current route -> %s', src);
-  }
+	if (dst) {
+		re = pathToRegexp(src, keys);
+		map = toMap(keys);
+		debug('rewrite %s -> %s    %s', src, dst, re);
+	} else {
+		debug('rewrite current route -> %s', src);
+	}
 
-  return function(req, res, next) {
-    var orig = req.url;
-    var m;
-    if (dst) {
-      m = re.exec(orig);
-      if (!m) {
-        return next();
-      }
-    }
-    req.url = (dst || src).replace(/\$(\d+)|(?::(\w+))/g, function(_, n, name) {
-      if (name) {
-        if (m) return m[map[name].index + 1];
-        else return req.params[name];
-      } else if (m) {
-        return m[n];
-      } else {
-        return req.params[n];
-      }
-    });
-    debug('rewrite %s -> %s', orig, req.url);
-    if (req.url.indexOf('?') > 0) {
-      req.query = URL.parse(req.url, true).query;
-      debug('rewrite updated new query', req.query);
-    }
-    if (dst) next();
-    else next('route');
-  }
+	return function(req, res, next) {
+		const orig = req.url;
+		let m;
+		if (dst) {
+			m = re.exec(orig);
+			if (!m) {
+				return next();
+			}
+		}
+		req.url = (dst || src).replace(/\$(\d+)|(?::(\w+))/g, (_, n, name) => {
+			if (name) {
+				if (m) return m[map[name].index + 1];
+				else return req.params[name];
+			} else if (m) {
+				return m[n];
+			} else {
+				return req.params[n];
+			}
+		});
+		debug('rewrite %s -> %s', orig, req.url);
+		if (req.url.indexOf('?') > 0) {
+			req.query = URL.parse(req.url, true).query;
+			debug('rewrite updated new query', req.query);
+		}
+		if (dst) next();
+		else next('route');
+	};
 }
 
 /**
@@ -71,12 +72,12 @@ function rewrite(src, dst) {
  */
 
 function toMap(params) {
-  var map = {};
+	const map = {};
 
-  params.forEach(function(param, i) {
-    param.index = i;
-    map[param.name] = param;
-  });
+	params.forEach((param, i) => {
+		param.index = i;
+		map[param.name] = param;
+	});
 
-  return map;
+	return map;
 }
